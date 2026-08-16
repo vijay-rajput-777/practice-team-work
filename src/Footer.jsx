ffe073bdced2e5ab6a8e6f040290e1fe86294c20
vijay-rajput-777 <virajrajput7861ag@gmail.com>
2026-08-16 08:39:28 +0530

    complete work

---
+++ src/Footer.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <section className='mainFooter'>
+
+        <span className='smlDiv'>
+          <span className='svgDiv'>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className='divSvg'><path d="M0-240v-63q0-43 44-70t116-27q13 0 25 .5t23 2.5q-14 21-21 44t-7 48v65H0Zm240 0v-65q0-32 17.5-58.5T307-410q32-20 76.5-30t96.5-10q53 0 97.5 10t76.5 30q32 20 49 46.5t17 58.5v65H240Zm540 0v-65q0-26-6.5-49T754-397q11-2 22.5-2.5t23.5-.5q72 0 116 26.5t44 70.5v63H780Zm-455-80h311q-10-20-55.5-35T480-370q-55 0-100.5 15T325-320ZM160-440q-33 0-56.5-23.5T80-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T160-440Zm640 0q-33 0-56.5-23.5T720-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T800-440Zm-320-40q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T600-600q0 50-34.5 85T480-480Zm0-80q17 0 28.5-11.5T520-600q0-17-11.5-28.5T480-640q-17 0-28.5 11.5T440-600q0 17 11.5 28.5T480-560Zm1 240Zm-1-280Z" /></svg>
+          </span>
+          <span className='experience'>
+            <p className='firstP'>50+</p>
+            <p className='secondP'>Happy clients</p>
+            <p className='thirdP'>Trusted by clients worldwide</p>
+          </span>
+        </span>
+
+        <span className='smlDiv'>
+          <span className='svgDiv'>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className='divSvg'><path d="m424-318 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h168q13-36 43.5-58t68.5-22q38 0 68.5 22t43.5 58h168q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm301.5-598.5Q510-807 510-820t-8.5-21.5Q493-850 480-850t-21.5 8.5Q450-833 450-820t8.5 21.5Q467-790 480-790t21.5-8.5ZM200-200v-560 560Z" /></svg>
+          </span>
+          <span className='experience'>
+            <p className='firstP'>30+</p>
+            <p className='secondP'>Projects Completed</p>
+            <p className='thirdP'>Successfully delivered</p>
+          </span>
+        </span>
+
+        <span className='smlDiv'>
+          <span className='svgDiv'>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className='divSvg'><path d="M240-280 40-480l200-200 56 56-143 144 143 144-56 56Zm178 132-76-24 200-640 76 24-200 640Zm302-132-56-56 143-144-143-144 56-56 200 200-200 200Z" /></svg>
+          </span>
+          <span className='experience'>
+            <p className='firstP'>10K</p>
+            <p className='secondP'>Lines of Code </p>
+            <p className='thirdP'>Clean & efficient code</p>
+          </span>
+        </span>
+
+        <span className='smlDiv'>
+          <span className='svgDiv'>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className='divSvg'><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z" /></svg>
+          </span>
+          <span className='experience'>
+            <p className='firstP'>5+year</p>
+            <p className='secondP'>Years of Experience </p>
+            <p className='thirdP'>In web development</p>
+          </span>
+        </span>
+
+        
+
+      </section>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/FtLast.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './FtLast.css'
+
+const FtLast = () => {
+  return (
+    <div>
+      <section className='mainFT'>
+        <span className='believe'>
+            <h4>our value</h4>
+            <h1>what we believe in </h1>
+        </span>
+
+              <span className='ourValue'>
+            <span className='divsvgg'>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className='divSvg'><path d="M468-240q-96-5-162-74t-66-166q0-100 70-170t170-70q97 0 166 66t74 162l-84-25q-13-54-56-88.5T480-640q-66 0-113 47t-47 113q0 57 34.5 100t88.5 56l25 84Zm48 158q-9 2-18 2h-18q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480v18q0 9-2 18l-78-24v-12q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93h12l24 78Zm305 22L650-231 600-80 480-480l400 120-151 50 171 171-79 79Z" /></svg>
+            </span>
+                  <span className='dtl'>
+            <h3>Quality First</h3>
+                  <p>
+                    We follow best practices to ensure high quality in every project we deliver.</p>
+                  </span>
+        </span>
+
+              <span className='ourValue'>
+              <span className='divsvgg'>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className='divSvg'><path d="M40-160v-160q0-34 23.5-57t56.5-23h131q20 0 38 10t29 27q29 39 71.5 61t90.5 22q49 0 91.5-22t70.5-61q13-17 30.5-27t36.5-10h131q34 0 57 23t23 57v160H640v-91q-35 25-75.5 38T480-200q-43 0-84-13.5T320-252v92H40Zm440-160q-38 0-72-17.5T351-386q-17-25-42.5-39.5T253-440q22-37 93-58.5T480-520q63 0 134 21.5t93 58.5q-29 0-55 14.5T609-386q-22 32-56 49t-73 17ZM160-440q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T280-560q0 50-34.5 85T160-440Zm640 0q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T920-560q0 50-34.5 85T800-440ZM480-560q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T600-680q0 50-34.5 85T480-560Z" /></svg>
+              </span>
+              <span className='dtl'>
+                <h3>Client Focused</h3>
+              <p>
+                Our cilents' success is our priority. We listan, plan and deliver the best
+                </p>
+                  </span>
+          </span>
+
+              <span className='ourValue'>
+              <span className='divsvgg'>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className='divSvg'><path d="m226-559 78 33q14-28 29-54t33-52l-56-11-84 84Zm142 83 114 113q42-16 90-49t90-75q70-70 109.5-155.5T806-800q-72-5-158 34.5T492-656q-42 42-75 90t-49 90Zm155-121.5q0-33.5 23-56.5t57-23q34 0 57 23t23 56.5q0 33.5-23 56.5t-57 23q-34 0-57-23t-23-56.5ZM565-220l84-84-11-56q-26 18-52 32.5T532-299l33 79Zm313-653q19 121-23.5 235.5T708-419l20 99q4 20-2 39t-20 33L538-80l-84-197-171-171-197-84 167-168q14-14 33.5-20t39.5-2l99 20q104-104 218-147t235-24ZM157-321q35-35 85.5-35.5T328-322q35 35 34.5 85.5T327-151q-25 25-83.5 43T82-76q14-103 32-161.5t43-83.5Zm57 56q-10 10-20 36.5T180-175q27-4 53.5-13.5T270-208q12-12 13-29t-11-29q-12-12-29-11.5T214-265Z" /></svg>
+              </span>
+                  <span className='dtl'>
+                  <h3>Continuous Growth </h3>
+              <p>
+                We keep learning and improving to stay ahead in technology.
+                </p>
+                  </span>
+          </span>
+
+        
+      </section>
+    </div>
+  )
+}
+
+export default FtLast
